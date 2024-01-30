0107d4ecd3c93eefcc0fad9f20a88eb4235a64cd
abigailridley <abigail.ridley@hotmail.co.uk>
2024-01-30 20:20:39 +0000

    Added homepage

---
+++ src/components/homepage/index.js
@@ -0,0 +1,21 @@
+import './style.css'; 
+
+
+
+const Homepage = () => {
+  return (
+<div>
+    <div className="homepage-container">
+    <div className='homepage-hero'>
+<h2>Abigail's</h2>
+<h1>Travel Blog</h1>
+    </div>
+    </div>
+   
+    </div>
+  
+
+  );
+};
+
+export default Homepage;

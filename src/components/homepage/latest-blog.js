ebf2cd1dbdc14720a715e96a6731834f1a48f383
abigailridley <abigail.ridley@hotmail.co.uk>
2024-02-24 14:53:43 -0800

    Update latest-blog.js

--- src/components/homepage/latest-blog.js
+++ src/components/homepage/latest-blog.js
@@ -6,9 +6,9 @@ import seattle from "./img/gtown-mansion.jpg";
 const LatestBlog = () => {
     return (
         <div className='latest-blog'>
-           <Link to='/seattle-blog'>
+           <Link to='/seattle-blog' className='blog-link-title'>
             <button type='button' className='latest-blog-button'>
-                <h4 className='blog-link-title'>Latest blog post</h4>
+                <h4 >Latest blog post</h4>
                 <img src={seattle} alt='img' className='latest-blog-img'/>
                 
             </button>

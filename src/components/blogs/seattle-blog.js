199ac1cff8380b66c9912c4dc21f41d2da0e78e1
abigailridley <abigail.ridley@hotmail.co.uk>
2024-02-23 16:11:56 -0800

    Create Seattle blog page

---
+++ src/components/blogs/seattle-blog.js
@@ -0,0 +1,11 @@
+import './seattle-blog.css'
+
+const SeattleBlog = () => {
+    return ( 
+        <div className="container seattle-blog">
+            <h1>Seattle Blog</h1>
+        </div>
+    )
+}
+
+export default SeattleBlog

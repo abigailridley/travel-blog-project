9aea3a99b153d0c1348f30a7d064ddf5312265f5
abigailridley <abigail.ridley@hotmail.co.uk>
2024-02-24 14:49:44 -0800

    update blog link css

--- src/components/homepage/latest-blog.js
+++ src/components/homepage/latest-blog.js
@@ -8,7 +8,7 @@ const LatestBlog = () => {
         <div className='latest-blog'>
            <Link to='/seattle-blog'>
             <button type='button' className='latest-blog-button'>
-                <h4>Latest blog post</h4>
+                <h4 className='blog-link-title'>Latest blog post</h4>
                 <img src={seattle} alt='img' className='latest-blog-img'/>
                 
             </button>

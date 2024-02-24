9f305284363f7b1feeab1760511758b1f130a3b8
abigailridley <abigail.ridley@hotmail.co.uk>
2024-02-23 16:12:22 -0800

    Update navbar - link to home & blog page

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,28 +1,30 @@
+import { Link } from 'react-router-dom';
+import './navbar.css'
+
 const Navbar = () => {
     return (
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/">Home</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">Blog</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/">Map</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/">About</a>
-              </li>
-              
-            </ul>
-          </div>
-        </div>
-      </nav>
+            <div class="container-fluid">
+                <Link class="navbar-brand home" to="/">Home</Link>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav nav-list">
+                        <li class="nav-item">
+                            <Link class="nav-link active" aria-current="page" to="/seattle-blog">Blog</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link" to="/">Map</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link" to="/">About</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
-  };
-  
-  export default Navbar;
+};
+
+export default Navbar;

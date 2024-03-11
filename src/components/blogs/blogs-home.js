cace6efe5a2e79e06616193eac2dae0bad9e5c7b
abigailridley <abigail.ridley@hotmail.co.uk>
2024-03-10 17:22:04 -0700

    blogs home css

--- src/components/blogs/blogs-home.js
+++ src/components/blogs/blogs-home.js
@@ -9,18 +9,20 @@ const Blogs = () => {
             <div className='blogs-container'>
                 <Link to='/portland-blog' className='blog-link'>
                     <button type='button' className='blog-button'> 
+                    <h5>Portland, OR</h5>
                         <img src={portland} alt='img' className='blog-img'/>
                         <div className='location'>
-                            <h5>Portland, OR</h5>
+                            
                             <p>25.02.24 - 03.03.24</p>
                         </div>
                     </button>
                 </Link> 
                 <Link to='/seattle-blog' className='blog-link'>
                     <button type='button' className='blog-button'>
+                    <h5>Seattle, WA</h5>
                         <img src={seattle} alt='img' className='blog-img'/>
                         <div className='location'>
-                            <h5>Seattle, WA</h5>
+                           
                             <p>17.02.24 - 25.02.24</p>
                         </div>
                     </button>

169224dd5d89149522f24b924d1501ad8c0a2c44
abigailridley <abigail.ridley@hotmail.co.uk>
2024-03-25 16:58:20 -0700

    Create footer component

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,12 +1,14 @@
-import MailchimpSubscribe from "react-mailchimp-subscribe"
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquare } from '@fortawesome/free-solid-svg-icons';
 
-
-const Footer= () => {
+const Footer = () => {
     return (
-<div>
-<MailchimpSubscribe url={process.env.REACT_APP_MAILCHIMP_URL} />
-</div>
-    )}
+        <div>
+            <MailchimpSubscribe url={process.env.REACT_APP_MAILCHIMP_URL} />
+            <FontAwesomeIcon icon={faSquare} />
+        </div>
+    );
+}
 
-
-    export default Footer;
+export default Footer;

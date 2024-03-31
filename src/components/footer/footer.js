9d14b61f1cfee9a8c1d860743e98bc409ebde5fa
abigailridley <abigail.ridley@hotmail.co.uk>
2024-03-31 16:38:30 -0700

    footer - come back here if breaks

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,12 +1,13 @@
 import MailchimpSubscribe from "react-mailchimp-subscribe";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSquare } from '@fortawesome/free-solid-svg-icons';
+import { faInbox } from '@fortawesome/free-solid-svg-icons';
+import './footer.css'
 
 const Footer = () => {
     return (
         <div>
-            <MailchimpSubscribe url={process.env.REACT_APP_MAILCHIMP_URL} />
-            <FontAwesomeIcon icon={faSquare} />
+           <div className="subscribe-form"> <MailchimpSubscribe url={process.env.REACT_APP_MAILCHIMP_URL} /> </div>
+            <FontAwesomeIcon icon={faInbox} />
         </div>
     );
 }

d59167177696421e90f6dd76be2cc3e86a799adc
abigailridley <abigail.ridley@hotmail.co.uk>
2024-05-22 11:58:25 -0600

    created basic WORKING backend with a form and home page

--- backend/blog_backend/app.py
+++ backend/blog_backend/app.py
@@ -1,11 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 from flask_migrate import Migrate
+from forms import BlogPostForm
 
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '\x96\xfb\xdb\xe1\xf4\x91\xc0\xd0\x90\x01\xae\xa8'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLACLHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -25,7 +27,19 @@ class Comment(db.Model):
 
 @app.route('/')
 def home():
-    return 'Hello, Flask'
+    posts = BlogPost.query.order_by(BlogPost.date.desc()).all()
+    return render_template('home.html', posts=posts)
+
+@app.route('/new_post', methods=['GET', 'POST'])
+def new_post():
+    form = BlogPostForm()
+    if form.validate_on_submit():
+        post = BlogPost(title=form.title.data, content=form.content.data)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', form=form)
 
 @app.route('/posts', methods=['GET', 'POST'])
 def handle_posts():

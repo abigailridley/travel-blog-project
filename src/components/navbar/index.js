92c95ebbbfc159d3d35caf339ef3ea7546cb35a8
abigailridley <abigail.ridley@hotmail.co.uk>
2024-11-14 12:55:46 +0000

    edit navlink active class names to comply with new handling rules in router

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,33 +1,76 @@
-import { NavLink } from 'react-router-dom';
-import './navbar.css'
+import { NavLink } from "react-router-dom";
+import "./navbar.css";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg">
-            <div className="container-fluid">
-                <NavLink exact className="navbar-brand home" activeClassName="active" to="/">home</NavLink>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav nav-list">
-                        <li className="nav-item">
-                            <NavLink exact className="nav-link" activeClassName="active" to="/blogs">blogs</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink exact className="nav-link" activeClassName="active" to="/map">map</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink exact className="nav-link" activeClassName="active" to="/about">about</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink exact className="nav-link" activeClassName="active" to="/subscribe">subscribe</NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "navbar-brand home active" : "navbar-brand home"
+          }
+          to="/"
+        >
+          home
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav nav-list">
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                to="/blogs"
+              >
+                blogs
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                to="/map"
+              >
+                map
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                to="/about"
+              >
+                about
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                to="/subscribe"
+              >
+                subscribe
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;

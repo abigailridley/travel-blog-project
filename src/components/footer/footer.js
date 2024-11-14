449392dcae994a047ba8b09f0571eb030e00757c
abigailridley <abigail.ridley@hotmail.co.uk>
2024-11-14 12:52:40 +0000

    restructure footer ensuring mailchimp works as expected

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,15 +1,21 @@
 import MailchimpSubscribe from "react-mailchimp-subscribe";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faInbox } from '@fortawesome/free-solid-svg-icons';
-import './footer.css'
+import "./footer.css";
 
 const Footer = () => {
-    return (
-        <div>
-           <div className="subscribe-form"> <div className="subscribe-msg"> <p>subscribe for email updates</p></div>  <MailchimpSubscribe url={process.env.REACT_APP_MAILCHIMP_URL} /> </div>
-           
-        </div>
-    );
-}
+  return (
+    <div>
+      <div className="subscribe-form">
+        {" "}
+        <div className="subscribe-msg">
+          {" "}
+          <p>subscribe for email updates</p>
+        </div>{" "}
+        <MailchimpSubscribe url={process.env.REACT_APP_MAILCHIMP_URL} />{" "}
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
